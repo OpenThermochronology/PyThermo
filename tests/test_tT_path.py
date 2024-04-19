bb5a62edb1e954da448cc5c1b2ddf459b5e3dfc6
Willy Guenthner <wrg@illinois.edu>
2024-04-19 07:59:59 -0500

    fixed tT path test issue

--- tests/test_tT_path.py
+++ tests/test_tT_path.py
@@ -12,20 +12,20 @@ def test_tT_interpolate(tT):
     
     assert np.all(interp_tT[:-1,0] >= interp_tT[1:,0])
     assert np.isnan(interp_tT).any() == False
-    assert np.any(interp_tT <= 0) == False
+    assert np.any(interp_tT < 0) == False
 
-def test_guenthner_anneal():
+def test_guenthner_anneal(tT):
     rho_r_array,relevant_tT = tT.guenthner_anneal()
 
     assert np.size(rho_r_array,0) == np.size(relevant_tT,0) and np.size(rho_r_array,1) == np.size(relevant_tT,0)
     assert np.isnan(rho_r_array).any() == False
-    assert np.any(rho_r_array <= 0) == False
+    assert np.any(rho_r_array < 0) == False
     assert np.any(rho_r_array > 1) == False
 
-def test_ketcham_anneal():
+def test_ketcham_anneal(tT):
     rho_r_array,relevant_tT = tT.ketcham_anneal()
 
     assert np.size(rho_r_array,0) == np.size(relevant_tT,0) and np.size(rho_r_array,1) == np.size(relevant_tT,0)
     assert np.isnan(rho_r_array).any() == False
-    assert np.any(rho_r_array <= 0) == False
+    assert np.any(rho_r_array < 0) == False
     assert np.any(rho_r_array > 1) == False
